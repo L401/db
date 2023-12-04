1c21a97e23e71e45b97d3dc80aa79a2fe1cec6b3
l401 <lvf401@gmail.com>
2023-12-04 11:45:21 -0600

    initial website layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import watchImage from './watch.jpg'
+import target from './target.png'
+import cart from './shopping-cart.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <img src={target} alt='Logo' width="50" height="50" />
+          <input type="search" placeholder="Search" />
+        <div>
+        <img src={cart} alt='Logo' width="30" height="30" />
+
+        </div>
+      </div>
+      <div className="flex-container">
+        {Array(6).fill().map((_, i) => (
+          <div key={i} className="flex-box">
+            <img src={watchImage} alt="Watch" style={{maxWidth: '75%', maxHeight: '75%'}} />
+            <button>Add to cart</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
